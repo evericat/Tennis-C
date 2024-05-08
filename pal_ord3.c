f9657f5d2b538ac4d0aef18ef5be2857663d04f6
Alfonso Sanchez Ferrer <sanchez4626@gmail.com>
2024-05-08 09:46:31 +0200

    Variables por parametro.
    Variables por parámetro indicadas. Analizaré si es necesario mediante reserva de memoria.

--- pal_ord3.c
+++ pal_ord3.c
@@ -1,19 +1,32 @@
 
 
 #include "winsuport2.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include "memoria.h"
 
 
-/* funcio per moure la paleta de l'ordinador; la paleta es mou en funcio del retard*/
-void *mou_paleta_ordinador(void *index) {
+/**
+ * @brief Funció que mou la paleta de l'ordinador.
+*/
+
+int main (int n_args, char *ll_args[]) {
   int f_h;
+
+  typedef struct {
+    int ipo_pf, ipo_pc;      	/* posicio del la paleta de l'ordinador */
+    float v_pal;			/* velocitat de la paleta del programa */
+    float pal_ret;			/* percentatge de retard de la paleta */
+    float po_pf;	/* pos. vertical de la paleta de l'ordinador, en valor real */
+  } Fila;
+
+  Fila *matrizPaletas = ll_args[0];
+  int *matrizMovimientosPaletas = ll_args[1];
+  int *index = ll_args[2];
+
  /* char rh,rv,rd; */
+  
   while ((tec != TEC_RETURN) && (cont == -1) && ((moviments > 0) || moviments == -1 || moviments_infinits == 1)) {
-    /**
-      * Per no posar sempre el mateix comentari: 
-      * pthread_mutex_lock(&sem); // Bloqueja el semafor
-      * // Codi que volem protegir
-      * pthread_mutex_unlock(&sem); // Desbloqueja el semafor
-    */
     win_retard(retard);
     f_h =matrizPaletas[*(int*)index].po_pf + matrizPaletas[*(int*)index].v_pal;		/* posicio hipotetica de la paleta */
     if (f_h != matrizPaletas[*(int*)index].ipo_pf)	/* si pos. hipotetica no coincideix amb pos. actual */
